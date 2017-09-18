8f369a844d72406603bfee300083e05f0e5a157c
Konior <arkadiusz.konior@nokia.com>
2017-09-18 08:07:58 +0300

    bug fix, new video

--- drive.py
+++ drive.py
@@ -9,6 +9,7 @@ import socketio
 import eventlet
 import eventlet.wsgi
 from PIL import Image
+import cv2
 from flask import Flask
 from io import BytesIO
 
@@ -61,6 +62,7 @@ def telemetry(sid, data):
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2BGR)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
